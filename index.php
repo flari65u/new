cb1babfb1c8164c679fd26c5991ecf2285710a7a
flari65u <140200611+flari65u@users.noreply.github.com>
2023-07-24 19:11:48 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,313 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="utf-8">
+	<title>Floating Ball</title>
+	    <!-- Favicon -->
+    <link rel="icon" href="img/products_new/6085.png" type="image/png">
+    <link rel="shortcut icon" href="img/products_new/6085.png" type="image/png">
+
+    <!-- OG -->
+    <meta property="og:url" content="http://<?php echo $_SERVER["HTTP_HOST"]; ?>/" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="Floating Ball" />
+    <meta property="og:image" content="http://<?php echo $_SERVER["HTTP_HOST"]; ?>/img/products_new/6085.png" />
+    <meta property="og:image:width" content="150" />
+    <meta property="og:image:height" content="150" />
+    <meta property="og:site_name" content="Floating Ball" />
+    <meta property="og:description" content="Floating Ball" />
+
+    
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<meta name=viewport content="width=device-width">
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+	
+	<!-- CSS -->
+	<link type="text/css" rel="stylesheet" href="files/floating_ball_1_IT/css/style.css"/>
+
+	<!-- JS -->
+	<script src="files/_js/jquery-2.2.4.min.js" type="text/javascript"></script>
+	<script type="text/javascript" src="files/_js/validation_IT.js"></script>
+	<script type="text/javascript" src="files/_js/ab.min.js"></script>
+	<script type="text/javascript" src="files/_js/timer.js"></script>
+  	<script type="text/javascript" src="files/floating_ball_1_IT/js/slick.min.js"></script>
+  	<script type="text/javascript" src="files/floating_ball_1_IT/js/script.js"></script>
+	<script type="text/javascript">$jsonData = {"product":{"id":6085,"name":"Стрелялка Floating Ball - EU (Европа)","title":"Floating Ball","short_description":"Floating Ball","full_description":"Floating Ball"},"prices":{"260":{"price":51,"old_price":102,"delivery_price":0,"tax_price":0,"upsale_price":0,"geo_key":"IT","name":"Italia","currency":"€","rate":0.017,"phone_template":"+39","address_template":"","name_template":"","active":true},"289":{"price":189,"old_price":378,"delivery_price":0,"tax_price":0,"upsale_price":0,"geo_key":"RO","name":"Romania","currency":"RON","rate":0.094,"phone_template":"+40767060312","address_template":"jud. Ilfov, Bucuresti, sector 3, sos. Balcani 15, scara. 2 ap.21","name_template":"Tudor Cristina","active":false},"294":{"price":83,"old_price":166,"delivery_price":0,"tax_price":0,"upsale_price":0,"geo_key":"BG","name":"България\t","currency":"BGN","rate":0.03,"phone_template":"+359895822542","address_template":"София гр., ул Николай Хайтов 65 вх.4 ет. 6 ап. 17","name_template":"Веселина Пеева","active":false}},"lowPrice":{"year":"2023","month":"07","day":"26"}};</script>
+
+
+</head>
+<body>
+<script type="text/javascript">
+  var landing_notifiers = new Object(),
+    price, currency, delivery_price;
+
+  for (key in $jsonData.prices) {
+    if ($jsonData.prices[key].active) {
+      price = $jsonData.prices[key].price;
+      currency = $jsonData.prices[key].currency;
+      delivery_price = $jsonData.prices[key].price + $jsonData.prices[key].delivery_price;
+    }
+  }
+
+  landing_notifiers.params = {
+    city: "",
+    landDir: "floating_ball_1_IT",
+    sex: "",
+    multiply: "" || 0,
+    no_price: "" || false,
+    delay: ("" * 1000) || 12000,
+    price: price || 0,
+    currency: currency || "руб.",
+    delivery_price: delivery_price,
+    geoKey: "" || "RU",
+    zdorov: "" || false
+  };
+</script>
+
+<script type="text/javascript" src="files/_blocks/notificators/func.js?11"></script>
+
+<script>
+  var formIsSubmitted = false;
+</script>
+
+<script type="text/javascript" src="files/_blocks/notificators/unload_submit.js?7"></script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                                                                                                                                                                          
+                    
+<div class=wrap>
+    <section class=offer>
+        <img class=logo src="files/floating_ball_1_IT/img/download.jpg">
+        <div class=price>
+            <div class=item>
+                <p>Prezzo <br> comune:<span style='display:none'>4b1e7fa2d9</span></p>
+                <div class=value><span><span class=price_land_s4><span class="price_land_s2">102</span> </span><span style='display:none'>c5ca56bb6d</span></span> <i><span class=price_land_curr> <span class="price_land_curr">€</span><span style='display:none'>d6d5be50ac</span></span><span style='display:none'>c15966ee3d</span></i></div>
+            </div>
+            <div class=item>
+                <p>Prezzo <br> promozionale:</p>
+                <div class=value><span><span class="price_land_s1">51</span><span style='display:none'>68fca0ff37</span></span> <i><span class=price_land_curr> <span class="price_land_curr">€</span></span></i></div>
+            </div>
+            <a href="#order" class=button-m>Ordina subito</a>
+        </div>
+        <div class=info>
+            <h1 class=verified>Floating Ball</h1>
+            <ul class=bullet>
+                <li>✅ La pistola per bambini e non solo!</li>
+                <li>✅ Spara il bersaglio, usando i proiettili 👍</li>
+                <li>✅ Accetta la sfida, giocate con gli amici!</li>
+            </ul>
+        </div>
+    </section>
+    <section class=post>
+        <div class=topTxt>
+            <p>NOVITÀ DELL’ANNO 2020 <span><span style='display:none'>cdae6fbf6d</span>SBRIGATI AD ORDINARE!</span></p>
+            <img class=im src="files/floating_ball_1_IT/img/11.gif" style="margin-bottom: -5px;  width: 480px;" alt="">
+        </div>
+        <div class=desc>
+            <div class=like> Like: 254 824</div>
+            <p>
+                <b> Floating Ball</b> Hai già scelto nuovi tipi di divertimento? Ti offriamo il più recente gioco interattivo, in cui possono partecipare tutti i membri della tua famiglia!
+            </p>
+            <div class=date>3 giorni</div>
+        </div>
+    </section>
+    <section class=post>
+        <div class=info>
+            <img class=logo src="files/floating_ball_1_IT/img/download.jpg">
+            <div class=login>Floating Ball</div>
+            <a href="#order" class=button-m>Ordina subito</a>
+        </div>
+        <img class=im src="files/floating_ball_1_IT/img/480xNxpost-im2.jpg.pagespeed.ic.qIe4QAqfb6.jpg" style="margin-bottom: -5px; width: 480px;" alt="">
+        <div class=desc>
+            <div class=like>Like: 215 320</div>
+            <p><b>Floating Ball – vivi un'esperienza emozionante di tiro verso un bersaglio!</b></p>
+            <ul class=list-v1>
+                <li>✅ È possibile regolare il livello di flusso d'aria in qualsiasi momento a seconda della difficoltà del gioco.🔫</li>
+                <li>✅ Ha la luce verde al neon quando fa buio.</li>
+                <li>✅ La console è dotata di diffusori musicali integrati che daranno al tuo gioco più divertimento ed energia. </li>
+                <li>✅ Migliora la coordinazione delle mani e degli occhi. </li>
+                <li>✅ I giocatori di tutte le età apprezzeranno sicuramente questo gioco, perché è molto interattivo ed innocuo.🏐</li>
+            </ul>
+            <div class=date>4 giorni</div>
+        </div>
+    </section>
+    <section class=post>
+        <div class=info>
+            <img class=logo src="files/floating_ball_1_IT/img/download.jpg">
+            <div class=login>Floating Ball</div>
+            <a href="#order" class=button-m>Ordina subito</a>
+        </div>
+        <div class=slider>
+            <div class=item>
+                <img style="width: 480px;" src="files/floating_ball_1_IT/img/slide1.gif" alt="">
+            </div>
+            <div class=item>
+                <img style="width: 480px;" src="files/floating_ball_1_IT/img/slide2.gif" alt="">
+            </div>
+            <div class=topTxt>
+                <p style="background-color:#c73b28;"> <br> SORPRENDI IL TUO BAMBINO </p>
+                <img style="width: 480px;" src="files/floating_ball_1_IT/img/slide3.gif" alt="">
+            </div>
+        </div>
+        <div class=desc>
+            <div class=like>Like: 243 265</div>
+            <p><b>Floating Ball - Come ordinare?<span style='display:none'>9bbad32a79</span></b><span style='display:none'>525c287801</span></p>
+            <ul class=list-v1>
+                <li>🚛 📦 Consegna tramite il corriere espresso entro 2-3 giorni.</li>
+                <li>💳 💵 L’ordine viene pagato esclusivamente alla consegna della merce.</li>
+                <li>💎 🔍 Controlliamo sempre la merce, garantiamo qualità al 100%! </li>
+            </ul>
+            <div class=date>5 giorni</div>
+        </div>
+    </section>
+    <section class=post>
+        <div class=info>
+            <img src="files/floating_ball_1_IT/img/xrev-ava1.jpg.pagespeed.ic.YTziwZGXBe.jpg" alt="">
+            <div class=login>_lorena_</div>
+        </div>
+        <img class=im src="files/floating_ball_1_IT/img/xrev-im1-1.jpg.pagespeed.ic.m0IqIbW1xe.jpg" alt="">
+        <div class=desc>
+            <div class=like>Like: 110 002</div>
+            <p>
+                <b>_lorena_</b> Il gioco è straordinario! L'ho preso una settimana fa per mio figlio. Ora non abbiamo pace nella nostra casa)). Sta sparando al bersaglio non-stop. 😀
+            </p>
+            <div class=date>5 giorni</div>
+        </div>
+    </section>
+    <section class=post>
+        <div class=info>
+            <img src="files/floating_ball_1_IT/img/xrev-ava2.jpg.pagespeed.ic.ewfA1VcBMj.jpg" alt="">
+            <div class=login>_paula@</div>
+        </div>
+        <img class=im src="files/floating_ball_1_IT/img/xrev-im2-2.jpg.pagespeed.ic.jf7xG_C0tC.jpg" alt="">
+        <div class=desc>
+            <div class=like>Like 89 776</div>
+            <p>
+                <b>_paula@</b> È un gioco perfetto. L'ho comprato per il compleanno del figlio.Le emozioni non hanno limiti. 😋 Nella prima settimana, il figlio stava giocando da solo, ora, anche nostro padre ha un nuovo intrattenimento. 😄
+            <span style='display:none'>09b758754c</span></p>
+            <div class=date>6 giorni </div>
+        </div>
+    </section>
+    <section class=post>
+        <div class=info>
+            <img src="files/floating_ball_1_IT/img/xrev-ava3.jpg.pagespeed.ic.jp6mOfWTQM.jpg" alt="">
+            <div class=login>bernardo_s</div>
+        </div>
+        <img class=im src="files/floating_ball_1_IT/img/xrev-im3-3.jpg.pagespeed.ic.WG05dM1T5P.jpg" alt="">
+        <div class=desc>
+            <div class=like>Like: 111 348</div>
+            <p>
+                <b><span style='display:none'>afb39982b0</span>bernardo_s</b> Ho comprato il gioco ai bambini per Capodanno,🎄 ma non ho avuto la pazienza di aspettare e, l'ho regalato subito. Non potete immaginare la loro reazione quando l'hanno ricevuto. Una gioia immensa. La figlia gioca quanto il figlio. Lo consiglio a tutti. Lo ordineremo per il nostro figlioccio a Capodanno.
+            <span style='display:none'>ccd3885450</span></p>
+            <div class=date>6 giorni</div>
+        </div>
+    </section>
+    <section class=order_form>
+        <div class=head>
+            <img class=logo src="files/floating_ball_1_IT/img/download.jpg">
+            <div class=login>Floating Ball</div>
+            <h3>Ordina subito</h3>
+        </div>
+        <div class=box>
+            <div class=timer_block>
+                <h4>La durata della promozione:</h4>
+                <div class="timer clrfix" id="timer">
+                    <div class=timer_item>
+                        <div class="count hours">00</div>
+                        <div class=text>ore</div>
+                    </div>
+                    <div class=timer_item>
+                        <div class="count minutes">00</div>
+                        <div class=text>minuti</div>
+                    </div>
+                    <div class=timer_item>
+                        <div class="count seconds">00</div>
+                        <div class=text>secondi</div>
+                    </div>
+                </div>
+            </div>
+            <div class=price>
+                <div class=item>
+                    <p><span style='display:none'>655cad5492<span style='display:none'>a7d3cdbda1</span></span>Prezzo comune:</p>
+                    <div class=value><span><span class=price_land_s4><span class="price_land_s2">102</span> </span><span style='display:none'>6faca875ad</span></span> <i><span class=price_land_curr> <span class="price_land_curr">€<span style='display:none'>b427d9776b</span></span></span><span style='display:none'>035ab04b1e</span></i></div>
+                </div>
+                <div class=item>
+                    <p>Prezzo promozionale:</p>
+                    <div class=value><span><span class=price_land_s1><span class="price_land_s1">51</span></span></span> <i><span class=price_land_curr> <span class="price_land_curr">€</span></span><span style='display:none'>113ced39d6</span></i></div>
+                </div>
+            </div>
+
+            <form action="order.php" class="m1-form orderformcdn" id="order" method="post">
+<input type="hidden" name="sub1" value="<?php echo !empty($_GET['utm_source']) ? htmlspecialchars($_GET['utm_source']) : ''; ?>">
+<input type="hidden" name="sub2" value="<?php echo !empty($_GET['utm_medium']) ? htmlspecialchars($_GET['utm_medium']) : ''; ?>">
+<input type="hidden" name="sub3" value="<?php echo !empty($_GET['utm_campaign']) ? htmlspecialchars($_GET['utm_campaign']) : ''; ?>">
+<input type="hidden" name="sub4" value="<?php echo !empty($_GET['utm_content']) ? htmlspecialchars($_GET['utm_content']) : ''; ?>">
+<input type="hidden" name="sub5" value="<?php echo !empty($_GET['utm_term']) ? htmlspecialchars($_GET['utm_term']) : ''; ?>">
+<input type="hidden" name="fbpx" value="<?php echo !empty($_GET['fbpx']) ? htmlspecialchars($_GET['fbpx']) : ''; ?>">
+
+                                    <select class="country_select form-control country_chang" name="country" id='country'>
+                                                    <option value='260' selected="selected">Italia</option>
+                                                    <option value='289'>Romania</option>
+                                                    <option value='294'>България	</option>
+                                            </select>
+                
+                <input class=field type=text name=name placeholder="Inserisci il tuo nome">
+                <input class=field type=tel name=phone placeholder="Inserisci il numero di telefono">
+                <input class="button-m big" value="Ordina subito" type=submit> </form>
+
+            <input type="hidden" name="address" value="">
+            <input type="hidden" name="product_count" id="product_count" value="1">
+            <input type="hidden" name="number_product" value="6085">
+            <input type="hidden" name="split_test_id" 	value="0">
+<input type="hidden" name="split_test" 		value="0">
+<input type="hidden" name="split_test_host" value="">
+<input type="hidden" name="fchck"           value="d8bf53253b016d28565d11923aec74ec18a3ec39">
+<input type="hidden" name="ucfi" id="ucfi"  value="0">        </div>
+    </section>
+    <div class="copyright">
+                    <img src="files/_blocks/copyright/img/rekv.png" style="display:block;margin:0 auto">
+
+<div style="text-align: right"><img src="files/_blocks/copyright/img/1001transits.png" style="display: inline-block !important;" /></div>
+                <br>
+        <a href="EN" target="_blank">Privacy Policy<span style='display:none'>7a80858b5a</span></a>
+    </div>
+</div>
+
+	
+
+
+<?php if (!empty($_GET['fbpx'])): ?><!-- Facebook Pixel Code --><script>!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version="2.0";n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window, document,"script","https://connect.facebook.net/en_US/fbevents.js");fbq("init", "<?php echo htmlspecialchars($_GET['fbpx']); ?>");fbq("track", "PageView");</script><noscript><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=<?php echo htmlspecialchars($_GET['fbpx']); ?>&ev=PageView&noscript=1"/></noscript><!-- End Facebook Pixel Code --><?php endif; ?>
+<center><a href="privacy-policy.html">Privacy policy</a> | <a href="terms.html">Terms and Conditions</a></center></body>
+</html>
+
+
+
+
